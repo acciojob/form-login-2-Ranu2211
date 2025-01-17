dd7ca563bf3fd033583c5809dd750e6cccc205f4
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 20:04:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-//your JS code here. If required.
 let firstName = document.getElementById('firstName');
 let lastName = document.getElementById('lastName');
 let phone = document.getElementById('phoneNumber');
 let email = document.getElementById('email');
 let submit = document.getElementById('submit');
-submit.addEventListener('click',()=>{
-	const firstNameValue = firstName.value.trim();
-            const lastNameValue = lastName.value.trim();
-            const phoneValue = phone.value.trim(); // Access phone.value correctly
-            const emailValue = email.value.trim();
 
-            alert(`First Name: ${firstNameValue} Last Name: ${lastNameValue} Phone Number: ${phoneValue} Email ID: ${emailValue}`);
-            
-	
-})
+submit.addEventListener('click', () => {
+    firstName.name = "First Name";
+    lastName.name = "Last Name";
+    phone.name = "Phone Number";
+    email.name = "Email ID";
+     const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    const phoneValue = phone.value.trim();
+    const emailValue = email.value.trim();
+const alertMessage = `First Name: ${firstNameValue} Last Name: ${lastNameValue} Phone Number: ${phoneValue} Email ID: ${emailValue}`;
+    
+    alert(alertMessage);
+});
